44ea3eac845c061bf244cf7828b0001bc7cbb791
Alan Eduardo Presas Cisneros <alanpresas60@gmail.com>
2020-03-04 11:58:39 -0600

    Lab 2 continuacion

--- ScriptDB2.sql
+++ ScriptDB2.sql
@@ -15,6 +15,24 @@ begin
 	VALUES(@idVideo, @titulo,@repro,@url)
 end
 
+Create Procedure sp_video_modificar
+	@idVideo int,
+	@titulo varchar (100),
+	@repro int,
+	@url varchar(100)
+as
+begin
+UPDATE Video SET titulo = @titulo, reproducciones = @repro, url = @url WHERE @idVideo = idVideo
+end
+
+
+Create Procedure sp_video_eliminar
+	@idVideo int
+as
+begin
+	DELETE FROM video WHERE @idVideo = idVideo
+end
+
 EXEC sp_video_insertar 1,'Video', 1, 'x'
 Select * From Video
 
